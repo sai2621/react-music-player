adbc7cd7803346267b88d3b2236d985a401ebc02
sai250 <dsaibhargav567@gmail.com>
2020-10-30 15:25:16 +0530

    Updates

---
+++ precache-manifest.1829bccbaf57f20e11f855f7c7d7892a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f99805ccc36d6fed8d639b148c66f09",
+    "url": "/react-music-player/index.html"
+  },
+  {
+    "revision": "d2319d266b09482ab5d6",
+    "url": "/react-music-player/static/css/main.8789084e.chunk.css"
+  },
+  {
+    "revision": "5ab4c83389aa1833a787",
+    "url": "/react-music-player/static/js/2.a527d758.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-music-player/static/js/2.a527d758.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2319d266b09482ab5d6",
+    "url": "/react-music-player/static/js/main.df6bc123.chunk.js"
+  },
+  {
+    "revision": "326acc59821834d872cc",
+    "url": "/react-music-player/static/js/runtime-main.a5201cae.js"
+  }
+]);
